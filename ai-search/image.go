77c6388eb2caff6aefdb22515723de856877e770
Alexander Levinson <alexander.levinson.70@gmail.com>
2026-03-29 18:50:43 +0300

    16. Adding the Manhattan Cost to our generated images

--- ai-search/image.go
+++ ai-search/image.go
@@ -96,11 +96,29 @@ func (g *Maze) drawSquare(col Wall, p Point, img *image.RGBA, c color.Color, siz
 	}, image.Point{}, draw.Src)
 
 	if !col.Wall {
+		switch g.SearchType {
+		case DIJKSTRA:
+			g.printManhattanCost(p, color.Black, patch)
+		default:
+			// Do nothing
+		}
 		g.printLocation(p, color.Black, patch)
 	}
 
 	draw.Draw(img, image.Rect(x, y, x+size, y+size), patch, image.Point{}, draw.Src)
 
+}
+
+func (g *Maze) printManhattanCost(p Point, c color.Color, patch *image.RGBA) {
+	point := fixed.Point26_6{X: fixed.I(6), Y: fixed.I(17)}
+	d := &font.Drawer{
+		Dst:  patch,
+		Src:  image.NewUniform(c),
+		Face: basicfont.Face7x13,
+		Dot:  point,
+	}
+	n := Node{State: p}
+	d.DrawString(fmt.Sprintf("%d", n.ManhattanDistance(g.Start)))
 }
 
 // printLocation
