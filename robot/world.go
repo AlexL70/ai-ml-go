a6ebaf30669970bffa27aeb4377db1a348e0f4b4
Alexander Levinson <alexander.levinson.70@gmail.com>
2026-03-31 07:28:31 +0300

    26. Loading the Room configuration

--- robot/world.go
+++ robot/world.go
@@ -1,6 +1,11 @@
 package main
 
-import "time"
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"time"
+)
 
 const (
 	// Display characters
@@ -53,5 +58,65 @@ type RoomConfig struct {
 }
 
 func NewRoom(configFile string, animate bool) *Room {
-	return &Room{}
+	// Load from the json config file
+	roomConfig, err := LoadRoomConfig(configFile)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	// Convert room dimensions to grid cells
+	gridWidth := roomConfig.Width / cellSize
+	gridHeight := roomConfig.Height / cellSize
+	grid := make([][]Cell, gridWidth)
+	for i := range grid {
+		grid[i] = make([]Cell, gridHeight)
+		for j := range grid[i] {
+			grid[i][j] = Cell{Type: "dirty", Cleaned: false, Obstacle: false}
+		}
+	}
+	// Add walls
+	for i := range gridWidth {
+		grid[i][0] = Cell{Type: "wall", Obstacle: true, ObstacleName: "wall"}
+		grid[i][gridHeight-1] = Cell{Type: "wall", Obstacle: true, ObstacleName: "wall"}
+	}
+	for j := range gridHeight {
+		grid[0][j] = Cell{Type: "wall", Obstacle: true, ObstacleName: "wall"}
+		grid[gridWidth-1][j] = Cell{Type: "wall", Obstacle: true, ObstacleName: "wall"}
+	}
+
+	// TODO: Add furniture
+
+	// Count cleanable cells
+	cleanableCellCount := 0
+	for i := range grid {
+		for j := range grid[i] {
+			if !grid[i][j].Obstacle {
+				cleanableCellCount++
+			}
+		}
+	}
+
+	return &Room{
+		Grid:               grid,
+		Width:              gridWidth,
+		Height:             gridHeight,
+		CleanableCellCount: cleanableCellCount,
+		CleanedCellCount:   0,
+		Animate:            animate,
+	}
+}
+
+func LoadRoomConfig(fileName string) (*RoomConfig, error) {
+	// Read JSON
+	jsonData, err := os.ReadFile(fileName)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read config file: %w", err)
+	}
+	// parse JSON
+	var config RoomConfig
+	err = json.Unmarshal(jsonData, &config)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse config file: %w", err)
+	}
+	return &config, nil
 }
